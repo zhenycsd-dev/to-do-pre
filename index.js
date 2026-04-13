2ffa3b7a694b3a22155be919afeac6e5e6d76a42
zhenycsd-dev <f3dorenckozh@yandex.ru>
2026-04-13 18:10:13 +0500

    удаление

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ function createItem(item) {
   	const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
   	const editButton = clone.querySelector(".to-do__item-button_type_edit");
 	textElement.textContent = item;
+	deleteButton.addEventListener("click", function() {
+		clone.remove();
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	});
+	
 	return clone;
 }
 
